a9dcd042e4bc95f27e9240fa1aeca6a0c0de17f9
Kadds <ack49@cam.ac.uk>
2019-01-18 11:26:35 +0000

    Added the appropriate legal headers

--- ports/ad4050/src/main.c
+++ ports/ad4050/src/main.c
@@ -1,9 +1,16 @@
-// Based on Code from Analog Devices and Micropython
+///////////////////////////////////////////////////////////////////////////////////
+// A. Kadis, Uni of Cambridge Physical Computation Lab 18th December 2018
+// This is the main executable file to generate the port of microptyhon for the AD4050
+// The code is heavily based on code taken from Analog Devices and Micropython,
+// their respective headers and license decalarations have been maintained below
+///////////////////////////////////////////////////////////////////////////////////
 
+
+
+
+
+// Analog Devices License Header
 /*! *****************************************************************************
- * @file    blinky_example.c
- * @brief   Example showing how to use the GPIO driver to blink LEDs as outputs.
- -----------------------------------------------------------------------------
 Copyright (c) 2017 Analog Devices, Inc.
 
 All rights reserved.
@@ -41,8 +48,43 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
-
 *****************************************************************************/
+
+
+
+
+
+// Micropython License Header
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013-2018 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+
+
+
 
 // Standard Library
 #include <stdint.h>
